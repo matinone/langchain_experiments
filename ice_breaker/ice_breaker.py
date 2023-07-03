a5341d01ce8043f672bb2591282de21a33cec48c
mbrignone <mnbrignone@gmail.com>
2023-07-03 16:59:36 -0300

    Create CLI using Typer

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import typer
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -10,7 +12,9 @@ from agents.linkedin_agent import scrape_linkedin_profile, get_linkedin_url
 from output_parsers import person_info_parser, PersonInformation
 
 
-def generate_ice_breaker(name: str) -> dict[str, PersonInformation | str]:
+def generate_ice_breaker(
+    name: str, fake_data: bool = True
+) -> dict[str, PersonInformation | str]:
     summary_template = """
         Given the LinkedIn information {information} about a person, I need you to create:
         1. A short summary of the person.
@@ -33,16 +37,18 @@ def generate_ice_breaker(name: str) -> dict[str, PersonInformation | str]:
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
     profile_url = get_linkedin_url(name=name)
-    linkedin_data = scrape_linkedin_profile(profile_url, fake_data=True)
+    linkedin_data = scrape_linkedin_profile(profile_url, fake_data=fake_data)
     result = chain.run(information=linkedin_data)
 
-    return {
+    final_result = {
         "person_information": person_info_parser.parse(result),
         "profile_pic_url": linkedin_data.get("profile_pic_url"),
     }
 
+    print(final_result)
+
+    return final_result
+
 
 if __name__ == "__main__":
-    name = "Matias Nicolas Brignone"
-    ice_breaker = generate_ice_breaker(name=name)
-    print(ice_breaker)
+    typer.run(generate_ice_breaker)
