b83358fc1809fa4262a5df2ad8f5a7c5fbb70c79
mbrignone <mnbrignone@gmail.com>
2023-07-03 16:05:59 -0300

    Add more requirements to the prompt

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,16 +1,20 @@
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
-from agents.linkedin_lookup_agent import scrape_linkedin_profile, get_linkedin_url
+from agents.linkedin_agent import scrape_linkedin_profile, get_linkedin_url
 
-load_dotenv()
 
 summary_template = """
     Given the LinkedIn information {information} about a person, I need you to create:
     1. A short summary of the person.
     2. Two interesting facts about the person.
+    3. A topic that may be of interest for that person.
+    4. A create Ice Breaker phrase to start a conversation with that person.
 """
 
 summary_prompt_template = PromptTemplate(
